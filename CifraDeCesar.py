c87fffbae85a892492b69332a14717305d323da7
Bryan Lucas <bryanlukas1997@gmail.com>
2019-03-11 19:07:58 -0300

    Devaneio

--- CifraDeCesar.py
+++ CifraDeCesar.py
@@ -3,35 +3,30 @@ import string
 def alfabeto():
     return list(string.ascii_uppercase)
 
-def cifrar(mensagem, chave):
+def cifrar(mensagem, chave, operacao):
     vetor= alfabeto()
     resp = []
     vetor_msg = list(mensagem)
     
-    for i in range(len(vetor_msg)):
-        if(vetor_msg[i] == " "):
-            resp.append(" ")
-        
-        for a in range(len(vetor)):
-            if(vetor_msg[i] == vetor[a]):
-                resp.append(vetor[(a + chave)%26])
+    if(operacao == 1):
+        for i in range(len(vetor_msg)):
+            if(vetor_msg[i] == " "):
+                resp.append(" ")
+            
+            for a in range(len(vetor)):
+                if(vetor_msg[i] == vetor[a]):
+                    resp.append(vetor[(a + chave)%26])
+    elif(operacao == 2):
+        for i in range(len(vetor_msg)):
+            if(vetor_msg[i] == " "):
+                resp.append(" ")
+            
+            for a in range(len(vetor)):
+                if(vetor_msg[i] == vetor[a]):
+                    resp.append(vetor[(a - chave)%26])
                 
     return "".join(resp)
     
-def decifrar(mensagem, chave):
-    vetor= alfabeto()
-    resp = []
-    vetor_msg = list(mensagem)
-    
-    for i in range(len(vetor_msg)):
-        if(vetor_msg[i] == " "):
-            resp.append(" ")
-        
-        for a in range(len(vetor)):
-            if(vetor_msg[i] == vetor[a]):
-                resp.append(vetor[(a - chave)%26])
-                
-    return "".join(resp)
 
 while(True):
     operacao = int(input("** CIFRAR - 1 | DECIFRAR - 2 ** "))
@@ -39,9 +34,9 @@ while(True):
     chave    = int(input("INSIRA A CHAVE (ex. 1,2,3...): "))
 
     if(operacao == 1):
-        print("MENSAGEM CIFRADA: ", cifrar(mensagem.upper(), chave))
+        print("MENSAGEM CIFRADA: ", cifrar(mensagem.upper(), chave, operacao))
     elif(operacao == 2):
-        print("MENSAGEM DECIFRADA: ", decifrar(mensagem.upper(), chave))
+        print("MENSAGEM DECIFRADA: ", cifrar(mensagem.upper(), chave, operacao))
         
     resp = input("NOVAMENTE? S / N ")
     
